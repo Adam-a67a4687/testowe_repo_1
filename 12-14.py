51741d18c94b4fe92cfa1446fb5921e91860ddce
Andrzej Szeszko <andrzejgustaw@gmail.com>
2023-04-03 13:41:29 +0200

    zmiana nazwy parametru w metodzie transfrm first

--- 12-14.py
+++ 12-14.py
@@ -1,7 +1,7 @@
 class Transformer():
 
-    def transform_first(self, xy_param):
-        return xy_param + 2023
+    def transform_first(self, xy_paramamters):
+        return xy_paramamters + 2023
     
     def transform2(self, x, y):
         z = 2 * (x + y)
