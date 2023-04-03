48872acfb9f8676a540a4b6bb9f6113d341bd60d
Andrzej Szeszko <andrzejgustaw@gmail.com>
2023-04-03 16:48:26 +0200

    zmiana nazwy parametry na xy_param

--- 12-14.py
+++ 12-14.py
@@ -1,7 +1,7 @@
 class Transformer():
 
-    def transform_first(self, xy_paramamters):
-        return xy_paramamters + 1000
+    def transform_first(self, xy_param):
+        return xy_param + 1000
 
     def transform2(self, x, y):
         z = 2 * (x + y)
