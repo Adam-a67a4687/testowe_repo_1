25c621ca979e9da3008529849faeb83f4bc39b88
Andrzej Szeszko <andrzejgustaw@gmail.com>
2023-04-03 16:46:27 +0200

    zmiana parametru z 2023 na 1000

--- 12-14.py
+++ 12-14.py
@@ -1,7 +1,7 @@
 class Transformer():
 
     def transform_first(self, xy_paramamters):
-        return xy_paramamters + 2023
+        return xy_paramamters + 1000
 
     def transform2(self, x, y):
         z = 2 * (x + y)
